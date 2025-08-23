a14bf60a35a222c84bca0a00526c1e2f1ef7d5be
aayushtirole <aayushtirole22@gmail.com>
2025-08-23 19:35:23 +0530

    update

--- Practice code basic/3.JS/SCS JS/variables & Declaration/script.js
+++ Practice code basic/3.JS/SCS JS/variables & Declaration/script.js
@@ -1,6 +1,47 @@
 // var let cont
 // declaration and initialization
 
-var a = 12;
 
-console.log(a); // 12
+// var concept 
+
+var a = 12;
+var a = 13; // redeclaring var is allowed
+
+// var window main add hota hain
+//  var= function scope hota hain 
+// hum fir se var ko declare kar sakte hain same name se and error nahi aayga 
+// var ko function ke andar bhi use kar sakte hain
+function test() {
+    var a = 20;
+    console.log(a); // 20
+}
+
+
+// let concept
+
+let a= 12;
+// let a = 13; // error: Identifier 'a' has already been declared
+
+
+// let = block scope hota hain
+// let ko fir se declare nahi kar sakte same name se
+// let ko block ke andar hi use kar sakte hain
+{
+    let a = 20;
+    console.log(a); // 20
+}
+
+
+
+// cont concept
+
+const a = 12;
+// const a = 13; // error: Identifier 'a' has already been declared
+
+// const = block scope hota hain
+// const main value change nahi kar sakte
+// const ko block ke andar hi use kar sakte hain
+{
+    const a = 20;
+    console.log(a); // 20
+}
