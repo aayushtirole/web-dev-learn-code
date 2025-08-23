8bd01ebdd42f17f03e7b9afe15ac472bcdb04926
aayushtirole <aayushtirole22@gmail.com>
2025-08-24 01:46:22 +0530

    var and dec end

--- Practice code basic/3.JS/SCS JS/01_variables_Declaration/06_test/script.js
+++ Practice code basic/3.JS/SCS JS/01_variables_Declaration/06_test/script.js
@@ -1,6 +1,23 @@
-let a = 10;
+let ab = 10;
 {
     let a = 20;
     console.log("INSIDE:",a); 
 }
-console.log("OUTSIDE:",a);
+console.log("OUTSIDE:",ab);
+
+
+// confusion
+
+if (true) {
+
+    var a = 1;
+    let b = 2;
+}
+console.log(a); // 1
+console.log(b); // reference error
+
+//  why cont allows changing object properties
+
+const person = { name: "aayush"};
+person.name = "Tirole"; // allowed // updation is allowed in const variable
+person = {}; // not allowed  // reassignment nhi kr sakte const variable main 
