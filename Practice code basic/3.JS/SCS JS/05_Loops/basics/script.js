67209e0f16c0b67494dc7e52631bc8b99044f638
aayushtirole <aayushtirole22@gmail.com>
2025-08-27 03:14:41 +0530

    loops initials

---
+++ Practice code basic/3.JS/SCS JS/05_Loops/basics/script.js
@@ -0,0 +1,6 @@
+// Loops
+
+// repeat karne ko loop kahte hain 
+// repeat something until a certain condition is met
+
+// for loop , while loop , do while loop , forin loop , forof loop,forEach loop
