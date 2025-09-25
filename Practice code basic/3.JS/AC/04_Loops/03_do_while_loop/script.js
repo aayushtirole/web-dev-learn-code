e53e337f73d113a375587f98c575e55567b70c62
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-09-26 00:52:29 +0530

    for of loop to loop

---
+++ Practice code basic/3.JS/AC/04_Loops/03_do_while_loop/script.js
@@ -0,0 +1,6 @@
+let =20;
+
+do {
+    console.log("Hello World", i);
+    i++;    
+}  while (i <= 5);
---
+++ Practice code basic/3.JS/AC/04_Loops/04_for_of_loops/script.js
@@ -0,0 +1,6 @@
+// for of loops string or array main mostly use hote hain 
+let firstName = "aayush";
+
+for (let char of firstName){
+    console.log(char);
+}
