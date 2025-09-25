113765a10fde64653ea6cc84422acf2f0a2210e0
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-09-25 23:47:31 +0530

    for loop to loop

---
+++ Practice code basic/3.JS/AC/04_Loops/01_For_Loop/script.js
@@ -0,0 +1,15 @@
+
+// for (let i = 1; i <= n; i++) {
+//     console.log("Hello World", i);
+// }
+
+// console.log("Loop Ended");
+
+// Sum of 1 to 5 
+
+let sum = 0;
+let n = prompt("Enter a number");
+for (let i = 1; i <= n; i++) {
+    sum = sum + i;          
+}
+console.log("Sum is :", sum);
