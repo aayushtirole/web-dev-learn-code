3cbcac9aac1eb2b7a9f776a90fb18d4d0ff9b6fa
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-09-26 20:41:59 +0530

    trim to string methods

--- Practice code basic/3.JS/AC/05_Strings/03_string_Methods/script.js
+++ Practice code basic/3.JS/AC/05_Strings/03_string_Methods/script.js
@@ -1,1 +1,18 @@
 // String Methods
+
+// Uppercase
+
+let str = "hello world";
+let upperStr = str.toUpperCase();
+console.log(upperStr); // Output: "HELLO WORLD"
+
+// Lowercase
+let newStr="HELLO WORLD";
+let lowerStr = upperStr.toLowerCase();
+console.log(lowerStr); // Output: "hello world
+
+// Trim (Remove whitespace from both ends -> Start and End)
+
+let spacedStr = "   Hello World   ";
+let trimmedStr = spacedStr.trim();
+console.log(trimmedStr); // Output: "Hello World"
