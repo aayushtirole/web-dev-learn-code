8cd99eb5f81a6eb6582ea2640c1a74b9f47359f7
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-09-26 10:11:35 +0530

    template literals to string

---
+++ Practice code basic/3.JS/AC/05_Strings/01_string/script.js
@@ -0,0 +1,13 @@
+// Strings 
+
+let str = "Hello World";
+
+console.log(str.length);// length property
+
+// indexing
+
+console.log(str[0]);   // indexing start from and can access by index
+
+// last character access
+// console.log(str[str.length - 1]);  // last character access by length -1 index
+console.log(str[str.length - 1]);
---
+++ Practice code basic/3.JS/AC/05_Strings/02_Template_Literals/script.js
@@ -0,0 +1,24 @@
+// Template Literals :- A way to have embedded expressions in strings
+
+// let specialString = `This is a template literal string`;
+
+// console.log(specialString);
+
+// Example
+
+
+
+let obj = {
+    item :"pen",
+    price : 10
+
+};
+
+let output = `The price of ${obj.item} is ${obj.price} rupees`;
+console.log(output);
+
+// Multiline string using template literals
+let multilineString = `This is line one
+This is line two
+This is line three`;        
+console.log(multilineString);
