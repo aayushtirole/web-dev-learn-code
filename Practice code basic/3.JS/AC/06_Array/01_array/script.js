f7d76b1ec792e53d65a22bdd5b643471e3a16ab0
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-09-27 14:09:05 +0530

    basic array to array

---
+++ Practice code basic/3.JS/AC/06_Array/01_array/script.js
@@ -0,0 +1,14 @@
+// Array 
+
+let marks = [90, 80, 70, 60, 50];
+
+console.log(marks); // Output: [90, 80, 70, 60, 50]
+console.log(marks[0]); // Output: 90 (Accessing the first element)
+console.log(marks.length); // Output: 5 (Length of the array)
+
+// Modifying an element
+marks[2] = 75;
+console.log(marks); // Output: [90, 80, 75, 60, 50]
+// Adding a new element
+marks[5] = 40;
+console.log(marks); // Output: [90, 80, 75, 60, 50, 40]
