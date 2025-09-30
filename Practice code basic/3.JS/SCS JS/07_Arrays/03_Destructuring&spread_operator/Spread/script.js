c80d29e7f1f65263b25b065baedeed302593c99a
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-01 00:59:23 +0530

    up

--- Practice code basic/3.JS/SCS JS/07_Arrays/03_Destructuring&spread_operator/Spread/script.js
+++ Practice code basic/3.JS/SCS JS/07_Arrays/03_Destructuring&spread_operator/Spread/script.js
@@ -5,7 +5,12 @@ let arr = [1,2,3,4,5,6,7,8,9];
 // '...' (spread operator) ka use karke ek naya array banaya
 let arr2 = [...arr];
 
+console.log(arr);  // Original array
 console.log(arr2); // [1,2,3,4,5,6,7,8,9]
+
+arr2[0] = 100; // arr2 ke pehle element ko change kar diya
+console.log(arr);  // Original array remains unchanged
+console.log(arr2); // arr2 ka pehla element ab 100 hai
 
 // Explanation 
 
