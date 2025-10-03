4553a679f659e84fbb98e2800799eb228fc2421b
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-04 02:12:01 +0530

    elementsbyid

---
+++ Practice code basic/3.JS/SCS JS/09_DOM/script.js
@@ -0,0 +1,13 @@
+//DOM : Document Object Model
+//Dom is a programming interface for HTML and XML documents.
+//DOM manupulation is a way to change the document structure, style, and content.
+//DOM is a tree-like structure where each node is an object representing a part of the document.
+//DOM can be manupulated using JavaScript to create dynamic and interactive web pages.
+
+
+//selecting elements
+//1.getElementById()
+//id ki help se element ko select krta hai
+let element=document.getElementById('click');
+console.log(element);
+
