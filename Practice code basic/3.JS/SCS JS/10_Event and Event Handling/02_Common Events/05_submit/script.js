8c15f588fa991c1fd38a4abd5313008171a3ab84
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-05 04:47:02 +0530

    up

--- Practice code basic/3.JS/SCS JS/10_Event and Event Handling/02_Common Events/05_submit/script.js
+++ Practice code basic/3.JS/SCS JS/10_Event and Event Handling/02_Common Events/05_submit/script.js
@@ -1,1 +1,20 @@
 //submit 
+
+let form = document.querySelector("form");
+
+let inputs = document.querySelectorAll("input");
+
+form.addEventListener("submit",function(dets){
+    dets.preventDefault();
+    
+
+
+    //to print the value of the form (indivitual values)
+    // console.log(
+    //     inputs[0].value,
+    //     inputs[1].value,
+    //     inputs[2].value,
+    //     inputs[3].value
+    // );
+});
+ 
