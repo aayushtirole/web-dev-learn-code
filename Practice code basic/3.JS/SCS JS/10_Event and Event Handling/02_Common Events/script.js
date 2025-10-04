45663c1f8e31a6e7bd6660c843750c1b737ab2d8
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-05 02:11:06 +0530

    change

--- Practice code basic/3.JS/SCS JS/10_Event and Event Handling/02_Common Events/script.js
+++ Practice code basic/3.JS/SCS JS/10_Event and Event Handling/02_Common Events/script.js
@@ -14,3 +14,13 @@ inp.addEventListener("input",function(dets){
         console.log(dets.data);
     }
 });
+
+//change 
+// change event tab chalta hain jab aapna koe input select ya text area main koe change ho jaye
+
+let sel = document.querySelector("select");
+let dev = document.querySelector("#device");
+sel.addEventListener("change",function(dets){
+    console.log(dets.target.value);
+    dev.textContent = `${dets.target.value} Device Selected`;
+});
