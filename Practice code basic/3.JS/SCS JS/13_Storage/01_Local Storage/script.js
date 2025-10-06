7646a96aa95c393e3e08094693d53fa91f27b83f
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-06 15:23:13 +0530

    local storage

---
+++ Practice code basic/3.JS/SCS JS/13_Storage/01_Local Storage/script.js
@@ -0,0 +1,16 @@
+//Local Storage 
+// 1. store kaise kare  -  setItem
+// 2. data fetch kaise kare - getItem
+// 3. remove kaise kare - removeItem
+// 4. update kaise kare - setItem hi update krna hain 
+
+// localStorage.setItem("name","aayush"); //store 
+
+// let val = localStorage.getItem("name");
+
+// localStorage.removeItem("name");
+
+
+// update karne ki process 
+
+localStorage.setItem("name","piyush");
