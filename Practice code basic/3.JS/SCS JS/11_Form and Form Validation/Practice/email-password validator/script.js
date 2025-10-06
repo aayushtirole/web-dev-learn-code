2bbc3e5d39632d79f82b2e8f4350e5e733a40e7a
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-06 14:37:23 +0530

    regex

--- Practice code basic/3.JS/SCS JS/11_Form and Form Validation/Practice/email-password validator/script.js
+++ Practice code basic/3.JS/SCS JS/11_Form and Form Validation/Practice/email-password validator/script.js
@@ -1,1 +1,17 @@
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let form = document.querySelector("form");
+
+form.addEventListener("submit",
+    function(dets){
  
+        dets.preventDefault();
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const pwdRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])[A-Za-z\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{8,32}$/;
+
+
+
+})
+
+
