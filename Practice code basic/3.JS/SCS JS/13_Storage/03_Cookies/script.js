4a53aa664878eca9903e99b51384c0dd2f247c09
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-06 15:41:46 +0530

    up

---
+++ Practice code basic/3.JS/SCS JS/13_Storage/03_Cookies/script.js
@@ -0,0 +1,8 @@
+//cookies : -  browser main chhota data store karne ke liye cookies ka istemaal hota hain 
+
+//cookies - 4kb
+// local storage and session storage - 5mb
+
+//cookies main jo bhi data store hoga wo dta page repload par automatically server par jaayega 
+
+document.cookie = "age = 26";
