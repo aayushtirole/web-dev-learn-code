9e736497ca9aca1bb920222fe2d1bda97efa6df2
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-19 00:25:16 +0530

    module pattern

--- Practice code basic/3.JS/SCS JS/19_Design Pattern/01_Module Pattern/script.js
+++ Practice code basic/3.JS/SCS JS/19_Design Pattern/01_Module Pattern/script.js
@@ -6,3 +6,34 @@
 
 // Is pattern ka main fayda hain data hiding (encapsulation) aur clean structure, taaki code secure, resuable aur anageable ban sake.
 
+// module pattern main jo bhi banayenge vo iife ke andar banayenge vo private ho jaata hain or hume iife ke andar hi return krna padhta ek object or fir uss object ko access kr skte hain 
+
+let Bank = (function(){
+   let bankbalance = 12000;
+
+   function checkbalance(){
+    console.log(bankbalance);
+   }
+
+   function setbalance(val){
+    bankbalance = val;
+   }
+
+   function withdraw(val){
+      if(val <= bankbalance){
+        bankbalance -= val;
+        console.log(bankbalance);
+      }
+   }
+
+   return{
+    checkbalance,
+    setbalance,
+    withdraw
+   }
+})();
+
+Bank.withdraw(1000);
+
+
+
