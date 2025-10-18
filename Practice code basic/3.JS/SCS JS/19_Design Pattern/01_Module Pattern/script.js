107e4005b2198bedd50b275ba128bf97a4d65577
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-18 22:02:43 +0530

    madule pattern theory

---
+++ Practice code basic/3.JS/SCS JS/19_Design Pattern/01_Module Pattern/script.js
@@ -0,0 +1,8 @@
+// Module Pattern
+
+// Module Pattern ek design pattern hain jisme hum apne code ek self executing function (IIFE) ke andar likhte hain, taki variables aur functions private rahen 
+
+//iske anadr se hum sirf wahi cheezein return karte ahin jo bahar use karni hain 
+
+// Is pattern ka main fayda hain data hiding (encapsulation) aur clean structure, taaki code secure, resuable aur anageable ban sake.
+
